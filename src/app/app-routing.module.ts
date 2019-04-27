c0ab6d1d14ffba947c4d072f8d95395642e382e9
masterhand83 <robinmiguelsdj@gmail.com>
2019-04-27 17:04:39 -0500

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StartComponent } from './start/start.component';
 import { LineaInfoComponent } from './linea-info/linea-info.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'start', pathMatch:'full'},
   {path: 'start', component: StartComponent},
-  {path: 'line', component: LineaInfoComponent}
+  {path: 'line', component: LineaInfoComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
 import { LineaInfoComponent } from './linea-info/linea-info.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
-    LineaInfoComponent
+    LineaInfoComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
