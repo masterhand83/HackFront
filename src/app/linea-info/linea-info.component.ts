d769b455f0e7552186a17eaac6aeddd047b6df6b
masterhand83 <robinmiguelsdj@gmail.com>
2019-04-28 08:56:09 -0500

    dfaarreglo

--- src/app/linea-info/linea-info.component.ts
+++ src/app/linea-info/linea-info.component.ts
@@ -20,21 +20,12 @@ export class LineaInfoComponent implements OnInit {
 
   ngOnInit() {
 
-<<<<<<< HEAD
-   
-    this.id=this.activatedRouted.snapshot.paramMap.get('id');
-
-    this.lineaService.getLinea(this.id).subscribe(res =>{
-      this.lineaService.linea=res as Linea[];
-      console.log(this.lineaService.linea);
-=======
     this.nombre= this.activatedRouted.snapshot.paramMap.get('nombre');
     this.id= this.activatedRouted.snapshot.paramMap.get('id');
     this.$linea = this.lineaService.getLinea(this.id);
     this.$linea.subscribe(res =>{
       console.log('dfsdf');
       console.log(res);
->>>>>>> aca4d3051f04b73b613c1341f2f397b9c1106593
     })
     console.log(this.$linea)
     //console.log(this.linea);
--- src/app/services/linea.service.ts
+++ src/app/services/linea.service.ts
@@ -7,37 +7,11 @@ import { Socket } from 'ngx-socket-io';
 @Injectable({
   providedIn: 'root'
 })
-
-
-
 export class LineaService {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
   linea:Linea[];
-
-  constructor(private http: HttpClient) { }
-  getLinea(_id:string){
-    return this.http.get(`http://${location.hostname}:3000/lines/stations/${_id}`);
-=======
-<<<<<<< HEAD
-  currentLinea = this.socket.fromEvent<Linea>('linea');
-  constructor(private http: HttpClient, private socket: Socket) { }
-  getLinea(_id: string): Observable<Linea>{
-    return this.http.get<Linea>(`http://${location.hostname}:3000/Linea?_id=${_id}`);
-=======
   constructor(private http: HttpClient) { }
   getLinea(_id:string): Observable<Linea>{
     return this.http.get<Linea>(`http://${location.hostname}:3000/lines/stations/${_id}`);
->>>>>>> 0126343e2eb99d461de263982bd88d0579134fd3
-  }
-  getLine(_id: string){
-    this.socket.emit('getLine',_id);
->>>>>>> 0d1bfe9ccdd4f088a1d74ba02c8b2b0a52881f14
-=======
-  constructor(private http: HttpClient) { }
-  getLinea(_id:string): Observable<Linea>{
-    return this.http.get<Linea>(`http://${location.hostname}:3000/lines/stations/${_id}`);
->>>>>>> aca4d3051f04b73b613c1341f2f397b9c1106593
   }
 }
