3a687d079fc8b851e1093365b399e979785b260e
adminjacob <adjamoro@gmail.com>
2019-06-12 18:21:30 -0500

    Twits implementados v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'HackFront';
+  route:any;
+
+  constructor(){
+
+  }
+
+  ngOnInit() {
+    this.route=window.location.pathname;
+    console.log(this.route);
+  }
+
+  ruta(index:any){
+    alert(index);
+  }
+  
+  
 }
+
